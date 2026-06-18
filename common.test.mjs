8d16f47f0a325343f7ba39358763ba74ee4814cc
Abdul Moiz <104764143+A-Moiz@users.noreply.github.com>
2026-06-18 22:22:10 +0100

    Completed tests

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,15 @@
-import { getGreeting } from "./common.mjs";
-import assert from "node:assert";
 import test from "node:test";
+import assert from "node:assert";
+import { calculateEventDay } from "./common.mjs";
 
-test("Greeting is correct", () => {
-  assert.equal(getGreeting(), "Hello");
+// Check if the function calculates the 2nd Tuesday of October 2024
+test("calculates the second Tuesday of October 2024 (Ada Lovelace Day)", () => {
+  const day = calculateEventDay(2024, "October", "Tuesday", "second");
+  assert.equal(day, 8);
+});
+
+// Check if the function calculates the 2nd Tuesday of October 2020
+test("calculates the second Tuesday of October 2020 (Ada Lovelace Day)", () => {
+  const day = calculateEventDay(2020, "October", "Tuesday", "second");
+  assert.equal(day, 13);
 });
